ac955374584531ee90fad25d33314e8d15eb04d1
Mohammed Riyaz <94772181+Riyaz1994@users.noreply.github.com>
2022-08-21 01:41:59 +0530

    created ansible_controller shell script

---
+++ ansible_controller.sh
@@ -0,0 +1,12 @@
+sudo apt-get update -y
+sudo apt-get dist-upgrade -y
+sudo apt-get install -y python2.7
+sudo apt-get install -y python-pip
+sudo apt-get install -y python3-pip
+sudo apt-get install python-boto -y
+sudo apt-get install python-boto3 -y
+sudo apt-get install -y software-properties-common
+sudo apt-add-repository ppa:ansible/ansible -y
+sudo apt-get update -y
+sudo apt-get install -y ansible
+sudo ansible --version
